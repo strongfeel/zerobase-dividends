1016ba25dcb595d93b299ba7bdbeb997412547d4
(GangPil_Song) <songgangpil@gmail.com>
2024-01-11 21:21:44 +0900

    file commit.

--- src/main/java/zerobase/dividends/persist/DividendRepository.java
+++ src/main/java/zerobase/dividends/persist/DividendRepository.java
@@ -2,6 +2,7 @@ package zerobase.dividends.persist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import zerobase.dividends.persist.entity.CompanyEntity;
 import zerobase.dividends.persist.entity.DividendEntity;
 
@@ -11,6 +12,9 @@ import java.util.List;
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
     List<DividendEntity> findAllByCompanyId(Long companyId);
+
+    @Transactional
+    void deleteAllByCompanyId(Long id);
 
     boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
--- src/main/java/zerobase/dividends/web/CompanyController.java
+++ src/main/java/zerobase/dividends/web/CompanyController.java
@@ -1,6 +1,7 @@
 package zerobase.dividends.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import zerobase.dividends.model.Company;
+import zerobase.dividends.model.constants.CacheKey;
 import zerobase.dividends.persist.entity.CompanyEntity;
 import zerobase.dividends.service.CompanyService;
 
@@ -17,6 +19,8 @@ import zerobase.dividends.service.CompanyService;
 public class CompanyController {
 
     private final CompanyService companyService;
+
+    private final CacheManager redisCacheManager;
 
     @GetMapping("/autocomplete")
     public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
@@ -44,8 +48,15 @@ public class CompanyController {
         return ResponseEntity.ok(company);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteCompany() {
-        return null;
+    @DeleteMapping("/{ticker}")
+    @PreAuthorize("hasRole('WRITE')")
+    public ResponseEntity<?> deleteCompany(@PathVariable String ticker) {
+        String companyName = this.companyService.deleteCompany(ticker);
+        this.clearFinanceCache(companyName);
+        return ResponseEntity.ok(companyName);
+    }
+
+    public void clearFinanceCache(String companyName) {
+        this.redisCacheManager.getCache(CacheKey.KEY_FINANCE).evict(companyName);
     }
 }
