d48add1309d232bc75a70c42cd92109afb9ddec0
(GangPil_Song) <songgangpil@gmail.com>
2024-01-11 20:18:17 +0900

    file commit.

--- src/main/java/zerobase/dividends/web/AuthController.java
+++ src/main/java/zerobase/dividends/web/AuthController.java
@@ -1,4 +1,38 @@
 package zerobase.dividends.web;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zerobase.dividends.model.Auth;
+import zerobase.dividends.security.TokenProvider;
+import zerobase.dividends.service.MemberService;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final MemberService memberService;
+
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+        // 회원 가입을 위한 API
+        var result = this.memberService.register(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request) {
+        // 로그인용 API
+        var member = this.memberService.authenticate(request);
+        var token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
+    }
 }
