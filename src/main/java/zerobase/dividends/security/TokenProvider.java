3791acd5cd314f2fc2d6d37c3e173a541bc0a9f5
(GangPil_Song) <songgangpil@gmail.com>
2024-01-11 21:12:11 +0900

    file commit.

--- src/main/java/zerobase/dividends/security/TokenProvider.java
+++ src/main/java/zerobase/dividends/security/TokenProvider.java
@@ -6,8 +6,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import zerobase.dividends.service.MemberService;
 
 import java.util.Date;
 import java.util.List;
@@ -18,6 +22,8 @@ public class TokenProvider {
 
     private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1시간
     private static final String KEY_ROLES = "roles";
+
+    private final MemberService memberService;
 
     @Value("${spring.jwt.secret}")
     private String secretKey;
@@ -41,6 +47,12 @@ public class TokenProvider {
                 .setExpiration(expiredDate) // 토큰 만료 시간
                 .signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
                 .compact();
+    }
+
+    public Authentication getAuthentication(String jwt) {
+        // Not implemented yet
+        UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUsername(String token) {
